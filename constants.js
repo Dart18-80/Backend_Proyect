acbbf0ca57b346666e7033562468f82b6054b2e8
Dart18-80 <diegoruiztrabanino13@gmail.com>
2023-04-24 11:05:33 -0600

    first commit

---
+++ constants.js
@@ -0,0 +1,17 @@
+const DB_USER = "Admin";
+const DB_PASSWORD = "2YOLD4p5acEhgH0u";
+const DB_HOST = "proyectmongos.ta9f3vu.mongodb.net";
+
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+
+const JWT_SECRET_KEY = "gR7cH9Svfj8JLe4c186Ghs48hheb3902nh5DsA";
+
+module.exports = {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  API_VERSION,
+  IP_SERVER,
+  JWT_SECRET_KEY,
+};
